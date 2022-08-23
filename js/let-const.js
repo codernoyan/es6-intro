cfd0564f49ee568ce029e271eccbbabb3e392eea
codernoyan <coder.noyan@gmail.com>
2022-08-23 21:08:25 +0600

    introduction to es 6 version of js

---
+++ js/let-const.js
@@ -0,0 +1,15 @@
+// no more var
+// let: let it to reassign
+// do not allow it to reassign
+
+let money = 25;
+money = 35;
+console.log(money);
+
+const bird = 'jaan pakhi';
+
+const message = bird + ' potas potas'
+
+// bird = 'moyna pakhi';
+// console.log(bird);
+console.log(message);
